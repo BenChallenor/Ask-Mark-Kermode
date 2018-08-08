24ee4c16515c94c68d85e60c05c0057485362ed1
Ben <ben.challenor@btopenworld.com>
2018-08-08 16:18:19 +0100

    search returns first result object

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const apiKey = config.API_KEY;
 
 $('document').ready(function() {
-      const video = {
-        api: function() {
-          $('.searchButton').click(function(event) {
-            event.preventDefault();
-              const film = ($('.inputBox').val());
-              $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=relevance&q=mark+kermode+reviews%22' + film + '%22&key=' + apiKey, function(data) {
-                  console.log(data);
-                });
-              });
-            }
+  const video = {
+    api: function() {
+      $('.searchButton').click(function(event) {
+        event.preventDefault();
+        const film = ($('.inputBox').val());
+        $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=relevance&q=mark+kermode+reviews%22' + film + '%22&key=' + apiKey, function(data) {
+          // console.log(data);
+          function results() {
+            const firstResult = data.items[0];
+            console.log(firstResult);
           }
-          video.api();
+          results();
         });
+      });
+    }
+  }
+  video.api();
+});
